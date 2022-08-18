10087a8bec2c38c8d2e2b9aaf6a4117487410aa1
Ceciliiajhernandez12 <111512047+Ceciliiajhernandez12@users.noreply.github.com>
2022-08-18 08:24:39 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+﻿const fetch = require("node-fetch");
+
+
+var URL="https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=perl&site=stackoverflow";
+
+fetch(URL)
+.then((respuesta)=>{
+  return respuesta.json()  
+}).then((resp)=>{
+ 
+    var con=0;
+    var con2=0;
+
+    for (a in resp.items){
+
+        if (resp.items[a].is_answered== true){
+            con++;
+        }
+        if (resp.items[a].is_answered== false){
+            con2++;
+        }
+    }
+    console.log("1 Obtener el número de respuestas contestadas y no contestadas");
+    console.log("preguntas contestadas "+con)
+    console.log("preguntas no contsetasdas "+con2)
+
+})
+
+
+
+
+
+fetch(URL)
+.then((respuesta)=>{
+  return respuesta.json()  
+}).then((resp)=>{
+ 
+var menor =resp.items[0].view_count
+var posicionm ;
+    for (a in resp.items){
+       
+        if (resp.items[a].view_count<= menor){
+
+            menor= resp.items[a].view_count;
+            posicionm=a;
+            
+        }       
+    }
+    console.log("3 Obtener la respuesta con menor número de vistas");
+    console.log(resp.items[posicionm]);
+
+})
+
+
+fetch(URL)
+.then((respuesta)=>{
+  return respuesta.json()  
+}).then((resp)=>{
+    var conmenor;
+    var conmayor;
+var inicio =new Date (resp.items[0].creation_date)
+    for (a in resp.items){
+      var compara =new Date (resp.items[a].creation_date);
+        if(inicio>compara){
+            conmenor=a
+        }
+        if(inicio<compara){
+            conmayor=a
+        }
+        
+    }
+    console.log("4. Obtener la respuesta más vieja y más actual ");
+
+    console.log("respuesta mas vieja ");
+    console.log(resp.items[conmenor] );
+    console.log("respuesta mas actual" );
+    console.log(resp.items[conmayor] );
+
+})                
+
+
+
